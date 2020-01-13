2c8ab9990b831a8763691d6c629bc995c916eba4
Sofia Prado <sofiaprado1@gmail.com>
2020-01-13 15:54:56 -0300

    eliminé comentarios innecesarios

--- app/src/main/java/edu/curso/android/swapi/StarshipDetail.java
+++ app/src/main/java/edu/curso/android/swapi/StarshipDetail.java
@@ -41,34 +41,6 @@ public class StarshipDetail extends AppCompatActivity {
 
         textViewLength = findViewById(R.id.textViewLength);
         textViewLength.setText("Largo: " + starship.getLength());
-
-
-/* Bundle extras = getIntent().getExtras();
-        String name = extras.getString("name");
-        String model = extras.getString("model");
-        String crew = extras.getString("crew");
-        String manufacturer = extras.getString("manufacturer");
-        String cost_in_credits = extras.getString("cost_in_credits");
-        String length= extras.getString("length");
-
-
-        textViewNameDetail = findViewById(R.id.textViewNameDetail);
-        textViewNameDetail.setText(name.toUpperCase());
-
-        textViewModelDetail = findViewById(R.id.textViewModelDetail);
-        textViewModelDetail.setText("Modelo: " + model);
-
-        textViewCrewDetail = findViewById(R.id.textViewCrewDetail);
-        textViewCrewDetail.setText("Tripulación: " + crew);
-
-        textViewManufacturer = findViewById(R.id.textViewManufacturer);
-        textViewManufacturer.setText("Fabricante " + manufacturer);
-
-        textViewCost = findViewById(R.id.textViewCost);
-        textViewCost.setText("Créditos: " + cost_in_credits);
-
-        textViewLength = findViewById(R.id.textViewLength);
-        textViewLength.setText("Largo: " + length);*/
     }
 }
 
